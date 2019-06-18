2e21f80a7b6efc66e2c02489e83535dd7b38bf7c
anewlens <36173414+anewlens@users.noreply.github.com>
2019-06-17 22:50:38 -0400

    +cardFooter+tightenedLayout

--- src/Display/Card.js
+++ src/Display/Card.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Activities from './Activities'
+import CardFooter from './CardFooter'
 
 const Card = ({day}) => {
     return (
-        <div className="card">
-            <div className="cardHeader">
+        <div className="card" id={day}>
+            <div className='cardHeader'>
                 <h2 className='day'>{day}</h2>
                 <button className='ellipses'></button>
             </div>
             <Activities />
+            <CardFooter />
         </div>
     )
 }
---
+++ src/Display/CardFooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CardFooter = props => {
+    return (
+        <div className='cardFooter'>
+            <p>X</p>
+            <p>&#x2714;</p>
+        </div>
+    )
+}
+
+export default CardFooter
