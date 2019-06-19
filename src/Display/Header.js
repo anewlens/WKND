1185a22703dfb761f027b65217e85caa3281dca0
anewlens <36173414+anewlens@users.noreply.github.com>
2019-06-18 20:35:44 -0400

    +SVGicons+interactiveIcons+animations

--- src/Display/Header.js
+++ src/Display/Header.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Header = props => {
     return (
         <header>
-            <div class="headerText">
+            <div className="headerText">
                 <h1 className="wkndTitle">WKND</h1>
                 <h4 className='dates'>June 14 - 16</h4>
             </div>
-            <div class="profileImg">
+            <div className="profileImg">
                 
             </div>
         </header>
