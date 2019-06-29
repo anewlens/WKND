f9fff3a8c99de28d11c57368b20b7118e8e26398
anewlens <36173414+anewlens@users.noreply.github.com>
2019-06-28 20:41:04 -0400

    +eliipses animation

--- src/Display/Card.js
+++ src/Display/Card.js
@@ -3,11 +3,15 @@ import Activities from './Activities'
 import CardFooter from './CardFooter'
 
 const Card = ({day}) => {
+    const handleMenu = e => {
+        e.target.classList.toggle('open')
+    }
+
     return (
         <div className="card" id={day}>
             <div className='cardHeader'>
                 <h2 className='day'>{day}</h2>
-                <button className='ellipses'></button>
+                <button className='ellipses' onClick={handleMenu}></button>
             </div>
             <Activities />
             <CardFooter day={day}/>
