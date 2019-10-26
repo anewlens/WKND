d69afacff473d9dffe91cdbdf73e4872ede10598
anewlens <36173414+anewlens@users.noreply.github.com>
2019-10-26 14:42:52 -0400

    db connection fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const db = new Sequelize(
-    `postgres://${process.env.DB_UN}:${process.env.DB_PW}@${process.env.DB_HOST}`
+    `postgres://${process.env.DB_UN}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 )
 
 module.exports = db
