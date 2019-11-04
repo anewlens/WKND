11abdc9815586dc48f16e270c4a1566e73970927
anewlens <36173414+anewlens@users.noreply.github.com>
2019-11-04 12:18:30 -0500

    Sequelize DataType Fix

--- models/Post.js
+++ models/Post.js
@@ -1,23 +1,23 @@
-const Sequelize = require('sequelize')
-const db = require('../config/db')
-const Comment = require('./Comment')
+const Sequelize = require("sequelize")
+const db = require("../config/db")
+const Comment = require("./Comment")
 
-const Post = db.define('post', {
-    text: {
-        type: Sequelize.STRING
-    },
-    day: {
-        type: Sequelize.STRING
-    },
-    createdAt: {
-        type: Sequelize.TIME
-    },
-    updatedAt: {
-        type: Sequelize.TIME
-    }
+const Post = db.define("post", {
+  text: {
+    type: Sequelize.STRING
+  },
+  day: {
+    type: Sequelize.STRING
+  },
+  createdAt: {
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    type: Sequelize.DATE
+  }
 })
 
-Post.hasMany(Comment, {foreignKey: 'post_id'})
-Comment.belongsTo(Post, {foreignKey: 'post_id'})
+Post.hasMany(Comment, { foreignKey: "post_id" })
+Comment.belongsTo(Post, { foreignKey: "post_id" })
 
 module.exports = Post
